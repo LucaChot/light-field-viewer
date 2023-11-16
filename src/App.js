7fdddf21ebd21bbc043c9c9dae280459920775fd
lucac-zilliqa <luca.choteborsky@outlook.com>
2023-11-16 00:18:00 +0000

    Implemented transformation between cameras

--- src/App.js
+++ src/App.js
@@ -17,9 +17,13 @@ const WaveShaderMaterial = shaderMaterial(
     precision mediump float;
 
     varying vec2 vUv;
+    varying mat4 V_k;
+    varying mat4 P_k;
 
     void main() {
       vUv = uv;
+      V_k = modelViewMatrix;
+      P_k = projectionMatrix;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
     }
   `,
@@ -30,10 +34,59 @@ const WaveShaderMaterial = shaderMaterial(
     uniform sampler2D uTexture;
 
     varying vec2 vUv;
+    varying mat4 V_k;
+    varying mat4 P_k;
+    // Camera properties
+    vec3 cam_pos = vec3(0.0, 0.0, 5.0);
+    vec3 lookAtPosition = vec3(0.0, 0.0, 0.0);
+    vec3 upDirection = vec3(0.0, 1.0, 0.0);
+
+    mat4 createViewMatrix(vec3 eye, vec3 target, vec3 up) {
+        vec3 f = normalize(target - eye);
+        vec3 r = normalize(cross(up, f));
+        vec3 u = cross(f, r);
+
+        return mat4(
+            vec4(r, 0.0),
+            vec4(u, 0.0),
+            vec4(-f, 0.0),
+            vec4(0.0, 0.0, 0.0, 1.0)
+        ) * mat4(
+            vec4(1.0, 0.0, 0.0, 0.0),
+            vec4(0.0, 1.0, 0.0, 0.0),
+            vec4(0.0, 0.0, 1.0, 0.0),
+            vec4(-eye, 1.0)
+        );
+    }
+
+    mat4 createProjection(vec3 N, vec3 w_f, mat4 viewMatrix) {
+      vec3 N_c = (viewMatrix * vec4(N, 1)).xyz;
+      vec3 w_f_c = (viewMatrix * vec4(w_f, 1)).xyz;
+      return mat4(
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(0.0, 1.0, 0.0, 0.0),
+        vec4(N_c, -dot(N_c, w_f_c)),
+        vec4(0.0, 0.0, 1.0, 0.0)
+      );
+    }
+
 
     void main() {
-      vec3 texture = texture2D(uTexture, vUv).rgb;
-      gl_FragColor = vec4(texture, 1.0); 
+      vec4 p_k = vec4(vUv * 2.0 - 1.0, 0 , 1);
+      vec3 N = vec3(0.0, 0.0, 1.0);
+      vec3 w_f = vec3(0.0, 0.0, 0.0);
+
+      mat4 V_i = createViewMatrix(cam_pos, lookAtPosition, upDirection);
+
+      mat4 P_k = createProjection(N, w_f, V_k);
+      mat4 P_i = createProjection(N, w_f, V_i);
+      vec4 p_i = P_i * V_i * inverse(V_k) * inverse(P_k) * p_k;
+      vec2 uv = (p_i.xy + 1.0) / 2.0;
+      vec3 tex = vec3(0.0, 0.0, 0.0);
+      if (uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0) {
+        tex = vec3(texture2D(uTexture, uv).rgb);
+      }
+      gl_FragColor = vec4(tex, 1.0); 
     }
   `
 );
@@ -49,15 +102,20 @@ const Screen = () => {
     //planeRef.current.position.copy(camera.position);
     
     // Make the plane look at the camera
-    planeRef.current.lookAt(camera.position);
+    //planeRef.current.lookAt(camera.position);
   });
   const [image] = useLoader(THREE.TextureLoader, [
     textureImg
   ]);
 
+  const verticalFOV = camera.fov * (Math.PI / 180); // Convert FOV to radians
+  const height = 2 * Math.tan(verticalFOV / 2) * camera.position.z;
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = height * aspect;
+
   return (
     <mesh ref={planeRef}>
-      <planeGeometry args={[1, 1, 16, 16]} />
+      <planeGeometry args={[width, height, 16, 16]} />
       <waveShaderMaterial uColor={"hotpink"} ref={planeRef} uTexture={image} />
     </mesh>
   );
